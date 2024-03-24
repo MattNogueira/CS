8497e183d339a05e96f32aad0fea44663953ffe0
Matheus Nogueira <mattnogdev@gmail.com>
2024-03-24 16:30:18 -0300

    Create Alg2.java

---
+++ EstruturasRepeticaoJava/Alg2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Alg2 {
+  public static void main(String[] args) {
+    // Declaração de variáveis//
+    Scanner teclado = new Scanner(System.in);
+    String nome, senha;
+    // Entrada de dados//
+    System.out.println("Digite seu nome de usuário: ");
+    nome = teclado.next();
+    System.out.println("Digite sua senha: ");
+    senha = teclado.next();
+    // Processamento e saída de dados//
+    while (nome.equals(senha)) {
+      System.out.println("Senha inválida, digite novamente: ");
+      senha = teclado.next();
+    }
+    System.out.println("Senha válida");
+    teclado.close();
+  }
+}
