2985309e7200e4447f9080a220172503f7dfe442
Matheus Nogueira <mattnogdev@gmail.com>
2024-03-24 16:02:45 -0300

    Create Alg1.java

---
+++ EstruturasRepeticaoJava/Alg1.java
@@ -0,0 +1,21 @@
+//Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.//
+
+import java.util.Scanner;
+
+public class Alg1 {
+  public static void main(String[] args) {
+    //Declaração de variáveis//
+    Scanner teclado = new Scanner(System.in);
+    int nota;
+    //Entrada de dados//
+    System.out.println("Digite uma nota entre 0 e 10: ");
+    nota = teclado.nextInt();
+    //Processamento e saída de dados//
+    while (nota < 0 || nota > 10) {
+      System.out.println("Nota inválida, digite novamente: ");
+      nota = teclado.nextInt();
+    };
+    System.out.println("Nota válida");
+    teclado.close();
+  }
+}
