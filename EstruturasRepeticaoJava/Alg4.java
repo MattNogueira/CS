c2c3f69f85281aeb4f13d9a2e115204b26c97067
Matheus Nogueira <mattnogdev@gmail.com>
2024-03-24 16:50:32 -0300

    Create Alg4.java

---
+++ EstruturasRepeticaoJava/Alg4.java
@@ -0,0 +1,14 @@
+public class Alg4 {
+  public static void main(String[] args) {
+    //Declaração de variáveis//
+    int populacaoA = 80000, populacaoB = 200000, anos = 0;
+    //Processamento//
+    while (populacaoA < populacaoB) {
+      populacaoA = (int) (populacaoA * 1.03);
+      populacaoB = (int) (populacaoB * 1.015);
+      anos++;
+    };
+    //Saída de dados//
+    System.out.println("Serão necessários " + anos + " anos.");
+  }
+}
