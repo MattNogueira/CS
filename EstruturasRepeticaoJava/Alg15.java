3d81c192f3e90d881cf62b1c386aebeb1167625a
Matheus Nogueira <mattnogdev@gmail.com>
2024-03-26 13:27:39 -0300

    Update Alg15.java

--- EstruturasRepeticaoJava/Alg15.java
+++ EstruturasRepeticaoJava/Alg15.java
@@ -6,12 +6,13 @@ public class Alg15 {
     System.out.print("Digite o número de termos da série de Fibonacci: ");
     int num = teclado.nextInt();
     int a = 1, b = 1, c = 0;
-    for (int i = 1; i <= num; i++) {
+    for (int i = 1; i < num; i++) {
       System.out.print(a + ", ");
       c = a + b;
       a = b;
       b = c;
     };
+    System.out.println(a);
     teclado.close();
   }
 }
