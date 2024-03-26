9166cf2a68675984bf0ea42fa242fd7f42688518
Matheus Nogueira <mattnogdev@gmail.com>
2024-03-26 12:17:49 -0300

    Create Alg8.java

---
+++ EstruturasRepeticaoJava/Alg8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Alg8 {
+  public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    int soma = 0;
+    for (int i = 1; i <= 5; i++) {
+      System.out.print("Digite o " + i + "º número: ");
+      int num = teclado.nextInt();
+      soma += num;
+    };
+    double media = (double) soma / 5;
+    System.out.println("A soma dos números é: " + soma);
+    System.out.println("A média dos números é: " + media);
+    teclado.close();
+  }
+}
