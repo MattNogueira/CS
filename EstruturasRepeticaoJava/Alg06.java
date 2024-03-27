bc951d287f2df1fd12d09d5d5fd513645c028f3b
Matheus Nogueira <mattnogdev@gmail.com>
2024-03-27 16:26:16 -0300

    Update Alg06.java

--- EstruturasRepeticaoJava/Alg06.java
+++ EstruturasRepeticaoJava/Alg06.java
@@ -1,9 +1,6 @@
 public class Alg06 {
   public static void main(String[] args) {
     for (int i = 1; i <= 20; i++) {
-      System.out.println(i);
-    };
-    for (int i = 1; i <= 20; i++) {
       System.out.print(i);
     };
   }
