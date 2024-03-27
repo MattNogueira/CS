31174f19643f4e2a371c0df491f9c198926e904a
Matheus Nogueira <mattnogdev@gmail.com>
2024-03-27 16:20:16 -0300

    Update and rename Alg6.java to Alg06.java

--- EstruturasCondicionaisJava/Alg06.java
+++ EstruturasCondicionaisJava/Alg06.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Alg6 {
+public class Alg06 {
   public static void main(String[] args) {
     //Declaração de variáveis//
     Scanner input = new Scanner(System.in);
