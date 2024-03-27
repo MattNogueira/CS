33460bf448695b663ed66553de469bc162700fbb
Matheus Nogueira <mattnogdev@gmail.com>
2024-03-27 16:19:29 -0300

    Update and rename Alg3.java to Alg03.java

--- EstruturasCondicionaisJava/Alg03.java
+++ EstruturasCondicionaisJava/Alg03.java
@@ -1,7 +1,7 @@
 //Importando o Scanner para entrada de dados//
 import java.util.Scanner;
 
-public class Alg3 {
+public class Alg03 {
   public static void main(String[] args) {
     //Declaração de variáveis//
     Scanner input = new Scanner(System.in);
