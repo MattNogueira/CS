0331661a87280e9e102111c439a20666694517c0
Matheus Nogueira <mattnogdev@gmail.com>
2024-03-27 16:25:38 -0300

    Update Alg07.java

--- EstruturasRepeticaoJava/Alg07.java
+++ EstruturasRepeticaoJava/Alg07.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class Alg07 {
   public static void main(String[] args) {
-    for (int i = 1; i <= 20; i++) {
-      System.out.println(i);
-    };
-    for (int i = 1; i <= 20; i++) {
-      System.out.print(i);
-    };
+    Scanner teclado = new Scanner(System.in);
+    int maior = 0;
+    for (int i = 1; i <= 5; i++) {
+      System.out.print("Digite o " + i + "º número: ");
+      int num = teclado.nextInt();
+      if (num > maior) {
+        maior = num;
+      }
+    }
+    System.out.println("O maior número é: " + maior);
+    teclado.close();
   }
 }
