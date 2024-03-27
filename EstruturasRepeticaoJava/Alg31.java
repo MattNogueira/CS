43daaf55356622d5c391aa353f4ec9b0711257a6
Matheus Nogueira <mattnogdev@gmail.com>
2024-03-27 13:57:56 -0300

    Create Alg31.java

---
+++ EstruturasRepeticaoJava/Alg31.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Alg31 {
+  public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    double preco, total = 0, entregue;
+    int i = 1;
+    do {
+      System.out.print("Digite o preço da mercadoria " + i + ": R$ ");
+      preco = teclado.nextDouble();
+      total += preco;
+      i++;
+    } while (preco != 0);
+    System.out.println("Total da compra: R$ " + total);
+    System.out.print("Digite o valor em dinheiro fornecido pelo cliente: R$ ");
+    entregue = teclado.nextDouble();
+    System.out.println("Troco: R$ " + (entregue - total));
+    teclado.close();
+  }
+}
