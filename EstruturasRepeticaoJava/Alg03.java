40ae5ed022db537be5ff5aeb790bcbab522ae568
Matheus Nogueira <mattnogdev@gmail.com>
2024-03-27 16:21:54 -0300

    Update Alg03.java

--- EstruturasRepeticaoJava/Alg03.java
+++ EstruturasRepeticaoJava/Alg03.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Alg3 {
+public class Alg03 {
   public static void main(String[] args){
     //Declaração de variáveis//
     Scanner teclado = new Scanner(System.in);
