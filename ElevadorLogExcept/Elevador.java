fe526fba513d1c2b777583db6506f935b3190560
Matheus Nogueira <mattnogdev@gmail.com>
2024-05-29 16:11:24 -0300

    Create Elevador.java

---
+++ ElevadorLogExcept/Elevador.java
@@ -0,0 +1,52 @@
+public class Elevador {
+  int andarAtual, totalAndares, capacidade, pessoasPresentes;
+
+  public void Inicializar(int tA, int c) {
+    andarAtual = 0;
+    totalAndares = tA;
+    capacidade = c;
+    pessoasPresentes = 0;
+
+    if (tA <= 0) {
+      throw new IllegalArgumentException("Total de andares inválido");
+    }
+    if (c <= 0) {
+      throw new IllegalArgumentException("Capacidade inválida");
+    }
+    if ((c <= 0)&&(tA <= 0)) {
+      throw new IllegalArgumentException("Total de andares e capacidade inválidos");
+    }
+  }
+
+  public void Entrar() {
+    if (pessoasPresentes < capacidade) {
+      pessoasPresentes += 1;
+    } else {
+      throw new IllegalArgumentException("Elevador lotado");
+    }
+  }
+
+  public void Sair() {
+    if (pessoasPresentes > 0) {
+      pessoasPresentes -= 1;
+    } else {
+      throw new IllegalArgumentException("Não há pessoas no elevador");
+    }
+  }
+
+  public void Subir() {
+    if (andarAtual < totalAndares) {
+      andarAtual += 1;
+    } else {
+      throw new IllegalArgumentException("Já está no último andar");
+    }
+  }
+
+  public void Descer() {
+    if (andarAtual > 0) {
+      andarAtual -= 1;
+    } else {
+      throw new IllegalArgumentException("Já está no térreo");
+    }
+  }
+}
